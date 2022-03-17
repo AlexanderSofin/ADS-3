b5771ac9d0f762c8aef15827a87d66c1d373f908
AlexanderSofin <99262399+AlexanderSofin@users.noreply.github.com>
2022-03-17 13:52:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,32 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int count = 0;
+	int left = 0, right = size;
+	int mid = (left + right) / 2;
+	while (left <= right) {
+		if (arr[mid] == value) {
+			count++;
+			break;
+		}
+		if (arr[mid] < value) {
+			left = mid + 1;
+			mid = (left + right) / 2;
+		}
+		if (arr[mid] > value) {
+			right = mid - 1;
+			mid = (left + right) / 2;
+		}
+	}
+	int l = 1;
+	while ((mid - l >= left) && (arr[mid - l] == value)) {
+		count++;
+		l++;
+	}
+	int r = 1;
+	while ((mid + r <= right) && (arr[mid + r] == value)) {
+		count++;
+		r++;
+	}
+	return count;
 }
